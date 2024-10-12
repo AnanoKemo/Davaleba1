0301ef4a6c93b37634d705cd7cbaffdde0d208cd
AnanoKemo <origami.georgia1@gmail.com>
2024-10-12 20:32:20 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="Info">
+      <h1>First Name: Anano</h1>
+      <h1>Last Name: Kemularia</h1>
+      <h1>Age: 14</h1>
+      <h1>Hobbies: painting,reading, writing, <p>playing drums, listening to music,</p> watching movies.</h1>
+    </div>
+  )
+   
+
+}
+export default App
